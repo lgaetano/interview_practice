9117d5d9901b8046382bc7a48a158def81ac184f
Lia Gaetano <lgaetano19@gmail.com>
2022-03-19 18:26:29 -0700

    Updates is_subset_palindrome conditions

--- arrays/valid_palindrome.py
+++ arrays/valid_palindrome.py
@@ -13,9 +13,6 @@ Output: True
 2, 2, 2, 2, 1, 3
 "tebbem"
 """
-
-from collections import Counter
-
 def is_subset_palindrome(s, i, j):
     while i < j:
         if s[i] != s[j]:
@@ -31,41 +28,12 @@ def is_palindrome(s):
 
     while l < r:
         if s[l] != s[r]:
-            return is_subset_palindrome(s, l, r - 1) or is_subset_palindrome(s, l - 1, r)
+            return is_subset_palindrome(s, l, r - 1) or is_subset_palindrome(s, l + 1, r)
 
         l += 1
         r -= 1
     
-    return False
-
-
-
-
-
-
-
-
-def validPalindrome(self, s: str) -> bool:
-        def check_palindrome(s, i, j):
-            while i < j:
-                if s[i] != s[j]:
-                    return False
-                i += 1
-                j -= 1
-            
-            return True
-
-        i = 0
-        j = len(s) - 1
-        while i < j:
-            # Found a mismatched pair - try both deletions
-            if s[i] != s[j]:
-                return check_palindrome(s, i, j - 1) or check_palindrome(s, i + 1, j)
-            i += 1
-            j -= 1
-        
-        return True
-
+    return True
 
 assert is_palindrome("deeee") == False
 assert is_palindrome("tebbem") == False
