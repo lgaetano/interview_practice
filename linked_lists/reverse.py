feddf9dd3c844a93b7102a615c6d8db63e00dc31
Lia Gaetano <lgaetano19@gmail.com>
2022-03-21 19:29:15 -0700

    Adds length and remove nth from end

--- linked_lists/reverse.py
+++ linked_lists/reverse.py
@@ -55,100 +55,58 @@ class LinkedList:
 
         self.head = prev
 
+    def length(self):
+        if self.head == None:
+            return 0
 
+        current = self.head
+        count = 0
 
+        while current:
+            count += 1
+            current = current.next
 
+        return count
+
+    def delete_nth_node_from_end(self, n):
+        # 1-2-3-4-5
+        length = self.length()
+
+        if n > length:
+            return None
+
+        elif n == length:
+            self.delete(self.head.value)
+
+        else:
+            search_length = length - n
+
+            current = self.head
+
+            for _ in range(search_length):
+                current = current.next
+
+            self.delete(current.value)
+            
 
 if __name__ == '__main__':
     ll = LinkedList()
 
-    nums = [1, 2, 3, 4]
+    nums = [1, 2, 3, 4, 5]
     for num in nums:
         ll.add_first(num)
 
-    ll.print_list()
+    # ll.print_list()
     print()
+
 
     ll.reverse()
     ll.print_list()
     print()
 
+    # ll.delete_nth_node_from_end(5)
+
     ll.delete(1)
     ll.print_list()
 
-
-
-
-# class Node: 
-#     def __init__(self, value):
-#         self.value = value 
-#         self.next = None
-
-# class LinkedList:
-#     def __init__(self):
-#         self.head = None
-
-#     def add_first(self, value):
-#         new_node = Node(value)
-
-#         new_node.next = self.head
-#         self.head = new_node
-
-#     def get_first(self):
-#         if not self.head:
-#             return None
-#         return self.head.value
-
-#     def length(self):
-#         count = 0 
-
-#         cur = self.head
-#         while cur:
-#             count += 1
-#             cur = cur.next
-#         return count
-        
-#     def get_last(self):
-#         if self.head == None:
-#             return None
-
-#         cur = self.head
-#         while cur.next:
-#             cur = cur.next
-#         return cur.value
-
-
-#     #  1 <- 2 <- 3 <- 4
-#     def reverse(self):
-#         if self.head == None:
-#             return None
-
-#         cur = self.head
-#         prev = None
-#         next = None
-        
-#         while cur:
-#             next = cur.next
-#             cur.next = prev
-#             prev = cur
-#             cur = next
-
-#         self.head = prev
-
-#     def print_ll(self):
-#         cur = self.head
-
-#         while cur:
-#             print(cur.value, end=" ")
-#             cur = cur.next
-
-# if __name__ == '__main__':
-#     ll = LinkedList()
-
-#     arr = [1, 2, 3, 4]
-#     for item in arr:
-#         ll.add_first(item)
-
-#     ll.print_ll()
-#     ll.reverse()
 
